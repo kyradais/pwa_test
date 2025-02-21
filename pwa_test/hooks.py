514307dbf6465c7bf5df9173c2a7db5752d20449
kyradais <kyradais@gmail.com>
2025-02-21 15:49:00 +0800

    Add new field

--- pwa_test/hooks.py
+++ pwa_test/hooks.py
@@ -5,15 +5,6 @@ app_description = "PWA Based Frappe Apps"
 app_email = "kyradais@gmail.com"
 app_license = "mit"
 
-fixtures = [
-    {
-        "dt": "DocType",
-        "filters": [
-            ["module", "=", "PWA Test"]
-        ]
-    }
-]
-
 
 # Apps
 # ------------------
